44abfbc4f785bccc2d20fa7106a63af92eb1ba2a
hanyidonggua@163.com <Zinsin0827>
2019-09-28 12:20:32 +0800

    20190928

--- student/views.py
+++ student/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
+def index(request):
+    students = Student.objects.all()
+    return render(request,'student/index.html',context={'students':students})
+
